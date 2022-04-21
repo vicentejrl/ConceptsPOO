5ce38f7d336fb30e1452ced33b9fe3b0c6d818d1
Vicente Romero <vicenterl.mx@hotmail.com>
2022-04-21 14:18:06 -0500

    Finish Date Class

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+
+try                 // Con Control+K+S saca el menú rodear con...
+{
+    Console.WriteLine(new Date(2000, 2, 29));
+    Console.WriteLine(new Date(2023, 2, 29));
+    Console.WriteLine(new Date(2022, 11, 31));
+}
+catch (Exception error)
+{
+    Console.Write(error.Message);
+}
